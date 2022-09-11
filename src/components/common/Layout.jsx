2b98a60e85c587fa0a052ee5ec792d1a553a2fbd
Felipe Mesa <pipemessi8@gmail.com>
2022-09-10 20:45:06 -0500

    feat: initial config

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,15 +1,17 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import './variables.css'
-import './global.css'
-import * as styles from './nav-bar.module.css'
+import "./variables.css";
+import "./global.css";
+import * as styles from "./nav-bar.module.css";
 
 const Layout = ({ children }) => {
   return (
     <div>
       {/* <Seo /> */}
-      <header className={styles.header} ><NavBar /></header>
-      <main style={{paddingTop:"100px"}}>{children}</main>
+      <header className={styles.header}>
+        <NavBar />
+      </header>
+      <main style={{ paddingTop: "100px" }}>{children}</main>
       <Footer />
     </div>
   );
--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
-import * as styles from './nav-bar.module.css'
+import * as styles from "./nav-bar.module.css";
 
 export default function NavBar() {
   const { error, data } = useQuery(MENU_NAVIGATIONS_QUERY);
@@ -12,22 +12,26 @@ export default function NavBar() {
     return <p>No Data!</p>;
   }
 
-  const {items} = data.menuNavigationCollection;
-  
+  const { items } = data.menuNavigationCollection;
+
   const menuOptions = (menu) => {
     return (
       menu &&
       menu.map((item) => (
         <Link
           className={styles.navigationItem}
-          activeClassName="active"
-          to={`/page?code=${item.menuPath}`}
+          key={item.menuPath}
+          to={
+            item.menuType === "SimplePageLink"
+              ? `/page?code=${item.menuPath}`
+              : item.menuPath
+          }
         >
           {item.name}
         </Link>
       ))
-    )
-  }
+    );
+  };
   return (
     <nav role="navigation" className={styles.container} aria-label="Main">
       <Link to="/" className={styles.logoLink}>
@@ -45,6 +49,7 @@ const MENU_NAVIGATIONS_QUERY = gql`
         code
         name
         menuPath
+        menuType
       }
     }
   }
